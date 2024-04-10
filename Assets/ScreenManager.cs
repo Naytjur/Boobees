94a2c739f4e086ca774669e1db6928a16a39e92f
Maksekse <122451932+Maksekse@users.noreply.github.com>
2024-04-10 21:58:34 +0200

    changes

---
+++ Assets/ScreenManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenManager : MonoBehaviour
+{
+    public GameObject collectionBook;
+    public GameObject beeScreen;
+    public GameObject beeInformationScreen;
+
+    public void BeeScreen();
+    
+    
+}
