7a17d17f52ca4ac740d545850d10196395f32271
Naytjur <jip.knottenbelt@gmail.com>
2024-04-13 20:23:54 +0200

    Flower setup
    Setup all the flowers for insect attraction and placing

--- Assets/Scripts/Planting/Plants/CowFieldWheat.cs
+++ Assets/Scripts/Planting/Plants/CowFieldWheat.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Buttercup : Plant
+public class CowFieldWheat : Plant
 {
 
 }
--- Assets/Scripts/Planting/Plants/CuckooFlower.cs
+++ Assets/Scripts/Planting/Plants/CuckooFlower.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Rose : Plant
+public class CuckooFlower : Plant
 {
 
 }
